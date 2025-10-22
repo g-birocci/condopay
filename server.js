7bfcffb2c304128da382c69839ef729cc04a8b74
g-birocci <gabrielbirocci@hotmail.com>
2025-10-22 18:28:47 +0100

    update

---
+++ server.js
@@ -0,0 +1,23 @@
+
+// ===== INICIALIZAÇÃO DO SERVIDOR (também não se deve mexer)=====
+
+app.use((req, res) => {
+  return handle(req, res);
+});
+
+const PORT = process.env.PORT || 3000;
+
+const iniciarServidor = async () => {
+  try {
+    await connectDB();
+    await nextApp.prepare();
+    app.listen(PORT, () => {
+      console.log(`Servidor Next.js + Express a correr em http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error('Erro ao iniciar servidor:', error);
+    process.exit(1);
+  }
+};
+
+iniciarServidor();
